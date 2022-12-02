ae2b100a823d6a41e03e3a488373671287dec6f0
Adnurhytia <adindanurhayati30@gmail.com>
2022-12-02 16:20:19 +0700

    Membuat method push pada element

--- Stack_Using_LinkedList/Program.cs
+++ Stack_Using_LinkedList/Program.cs
@@ -20,12 +20,23 @@ namespace StackUsingLinkedList_CSharp
         {
             top = null;
         }
+
         bool empty()
         {
             if (top == null)
                 return (true);
             else
                 return (false);
+        }
+
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed.")
         }
     }
 }
